9b1c4ca59211a697d988169ad9365a7b9f88c925
Zain Rizwan Khan <zainrizwankhan@gmail.com>
2020-12-07 22:46:09 +0000

    Added testbench files for project verification.

---
+++ verilog/dv/caravel/ghazi_top_dffram_csv/test_1/test_1.c
@@ -0,0 +1,34 @@
+#include "../../defs.h"
+
+void main()
+{
+  // reg_mprj_io_0 = GPIO_MODE_USER_STD_OUTPUT;
+  // reg_mprj_io_1 = GPIO_MODE_USER_STD_OUTPUT;
+  // reg_mprj_io_2 = GPIO_MODE_USER_STD_OUTPUT;
+  // reg_mprj_io_3 = GPIO_MODE_USER_STD_OUTPUT;
+  // reg_mprj_io_4 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_5 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+  // reg_mprj_io_6 = GPIO_MODE_USER_STD_OUTPUT;
+  // reg_mprj_io_7 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_6 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_7 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_8 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_9 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
+ 
+  reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
+
+  reg_mprj_xfer = 1;
+  while(reg_mprj_xfer == 1);
+	
+  reg_la1_ena = 0x00000000;
+  reg_la1_data = 0x0000015C;
+  
+  reg_la0_ena = 0x00000000;
+  reg_la0_data = 0x00000001;
+  reg_la0_data = 0x00000000;
+  reg_mprj_datah = 0x20;
+
+}
